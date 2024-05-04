fee9f713cf73eeea3108d598d5e462ac03bb094d
Gavin Acosta <155584291+Defiantearth@users.noreply.github.com>
2024-05-04 12:51:31 -0500

    Add files via upload

---
+++ javaScript.js
@@ -0,0 +1,16 @@
+var ourName = {
+    "name": "Camper",
+    "legs": 4, 
+    "tails": 1,
+    "friends": ["everything!"]
+}
+
+var myDog = {
+    "name": "Quincy",
+    "legs": 3, 
+    "tails": 2,
+    "friends": []
+}
+
+console.log(ourName["name"]);
+console.log(ourName["legs"]);
