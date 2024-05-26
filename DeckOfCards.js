22ca49f2a764a30be32a26a3a97c524c6e96af57
Gavin Acosta <155584291+Defiantearth@users.noreply.github.com>
2024-05-26 13:21:55 -0500

    Added rest of the cards for the deck

--- DeckOfCards.js
+++ DeckOfCards.js
@@ -23,19 +23,19 @@ function shuffleDeck(deck){
 
     //a list of objects. the list is the deck and each object is a card.
 const deckUnshuffled = [
-    {suite: "s", value: "A"},
-    {suite: "s", value: "2"},
-    {suite: "s", value: "3"},
-    {suite: "s", value: "4"},
-    {suite: "s", value: "5"},
-    {suite: "s", value: "6"},
-    {suite: "s", value: "7"},
-    {suite: "s", value: "8"},
-    {suite: "s", value: "9"},
-    {suite: "s", value: "10"},
-    {suite: "s", value: "J"},
-    {suite: "s", value: "Q"},
-    {suite: "s", value: "K"},
+    {suite: "s", value: "A"},{suite: "c", value: "A"},{suite: "h", value: "A"},{suite: "d", value: "A"},
+    {suite: "s", value: "2"},{suite: "c", value: "2"},{suite: "h", value: "2"},{suite: "d", value: "2"},
+    {suite: "s", value: "3"},{suite: "c", value: "3"},{suite: "h", value: "3"},{suite: "d", value: "3"},
+    {suite: "s", value: "4"},{suite: "c", value: "4"},{suite: "h", value: "4"},{suite: "d", value: "4"},
+    {suite: "s", value: "5"},{suite: "c", value: "5"},{suite: "h", value: "5"},{suite: "d", value: "5"},
+    {suite: "s", value: "6"},{suite: "c", value: "6"},{suite: "h", value: "6"},{suite: "d", value: "6"},
+    {suite: "s", value: "7"},{suite: "c", value: "7"},{suite: "h", value: "7"},{suite: "d", value: "7"},
+    {suite: "s", value: "8"},{suite: "c", value: "8"},{suite: "h", value: "8"},{suite: "d", value: "8"},
+    {suite: "s", value: "9"},{suite: "c", value: "9"},{suite: "h", value: "9"},{suite: "d", value: "9"},
+    {suite: "s", value: "10"},{suite: "c", value: "10"},{suite: "h", value: "10"},{suite: "d", value: "10"},
+    {suite: "s", value: "J"},{suite: "c", value: "J"},{suite: "h", value: "J"},{suite: "d", value: "J"},
+    {suite: "s", value: "Q"},{suite: "c", value: "Q"},{suite: "h", value: "Q"},{suite: "d", value: "Q"},
+    {suite: "s", value: "K"},{suite: "c", value: "K"},{suite: "h", value: "K"},{suite: "d", value: "K"}
 ];
 
 //shuffle deck
